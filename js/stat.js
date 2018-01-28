5c639de919cf8006cf59741b77451f2df7e1fca4
snpr <vaugk13@gmail.com>
2018-01-28 17:08:26 +0500

    Добавил надпись о победе

--- js/stat.js
+++ js/stat.js
@@ -5,4 +5,9 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = 'black';
+  ctx.font = 'PT Mono 16px';
+  ctx.fillText('Ура, вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 };
