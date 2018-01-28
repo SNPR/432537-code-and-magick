ddd26d4867c972c1a77e241137957930aa3ddbf5
snpr <vaugk13@gmail.com>
2018-01-28 23:06:31 +0500

    Убрал лишние параметры функций.

--- js/stat.js
+++ js/stat.js
@@ -11,8 +11,9 @@ window.renderStatistics = function (ctx, names, times) {
   var FONT_GAP = 90;
   var TIMES_GAP = 235;
 
-  /** @description Определеяет максимальное время прохождения игры и находит пропорцию.
-   * @param arr Массив с временами победы игроков.
+  /**
+   * @description Определеяет максимальное время прохождения игры и находит пропорцию.
+   * @param {object} arr Массив с временами победы игроков.
    * @return {number}
    */
   var defineMaxValue = function (arr) {
@@ -27,23 +28,19 @@ window.renderStatistics = function (ctx, names, times) {
 
   /**
    * @description Рисует облако.
-   * @param ctx Контекст canvas.
-   * @param x Координата x облака.
-   * @param y Координата y облака.
-   * @param color Цвет облака.
+   * @param {number} x Координата x облака.
+   * @param {number} y Координата y облака.
+   * @param {string} color Цвет облака.
    */
-  var renderCloud = function (ctx, x, y, color) {
+  var renderCloud = function (x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
   };
 
   /**
    * @description Рисует гистограммы.
-   * @param ctx Контекст canvas.
-   * @param names Массив с именами игроков.
-   * @param times Массив с временами победы игроков.
    */
-  var renderGists = function (ctx, names, times) {
+  var renderGists = function () {
     for (var i = 0; i < names.length; i++) {
       if (names[i] === 'Вы') {
         ctx.fillStyle = 'rgba(255, 0, 0, 1)';
@@ -55,8 +52,8 @@ window.renderStatistics = function (ctx, names, times) {
 
   };
 
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
+  renderCloud(CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(CLOUD_X, CLOUD_Y, 'white');
 
   ctx.fillStyle = 'black';
   ctx.font = 'PT Mono 16px';
@@ -64,11 +61,11 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, 60);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], BAR_HEIGHT - GAP + FONT_GAP * i, CLOUD_WIDTH - BAR_WIDTH * 4)
+    ctx.fillText(names[i], BAR_HEIGHT - GAP + FONT_GAP * i, CLOUD_WIDTH - BAR_WIDTH * 4);
   }
 
 
-  renderGists(ctx, names, times);
+  renderGists(names, times);
 
   ctx.fillStyle = 'black';
 
