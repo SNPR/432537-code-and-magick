6e8db3f8672f8f6b871e9a94ad27f70c957076e0
snpr <vaugk13@gmail.com>
2018-02-01 21:26:03 +0500

    Переделал функцию случайной сортировки элементов массива, добавил JSDoc

--- js/setup.js
+++ js/setup.js
@@ -4,96 +4,59 @@ var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARDS_TOTAL = 4;
 
-var randomizeArray = function (array) {
-  return array[Math.floor(Math.random() * array.length)];
+/**
+ * Вспомогательная функция для метода сортировки массива arr.sort.
+ * @return {number} Возвращает случайное число от -0.5 до 0.5.
+ */
+var compareRandom = function () {
+  return Math.random() - 0.5;
 };
 
-var randomWizardName = function (WIZARD_NAMES) {
-  var name = [];
-  for (var i = 0; i < 4; i++) {
-    name.push(randomizeArray(WIZARD_NAMES));
-    while (name[i] === name[i + 1] || name[i] === name[i + 2] || name[i] === name[i + 3] || name[i] === name[i - 1] || name[i] === name[i - 2] || name[i] === name[i - 3]) {
-      name.splice(i, 1);
-      name.push(randomizeArray(WIZARD_NAMES));
-    }
-  }
-  return name;
+/**
+ * Перемешивает значения массива случайным образом и возвращает массив длины WIZARDS_TOTAL.
+ * @param {Array} array Массив с любым типом данных.
+ * @param {number} WIZARDS_TOTAL Заданное число волшебников.
+ * @return {Array} Массив со случайным порядком значений.
+ */
+var randomizeArray = function (array, WIZARDS_TOTAL) {
+  var result = array.slice();
+  result.sort(compareRandom);
+  return result.splice(0, WIZARDS_TOTAL);
 };
 
-var names = randomWizardName(WIZARD_NAMES);
-
-
-var randomWizardSurname = function (WIZARD_SURNAMES) {
-  var surname = [];
-  for (var i = 0; i < 4; i++) {
-    surname.push(randomizeArray(WIZARD_SURNAMES));
-    while (surname[i] === surname[i + 1] || surname[i] === surname[i + 2] || surname[i] === surname[i + 3] || surname[i] === surname[i - 1] || surname[i] === surname[i - 2] || surname[i] === surname[i - 3]) {
-      surname.splice(i, 1);
-      surname.push(randomizeArray(WIZARD_SURNAMES));
-    }
-  }
-  return surname;
-};
-
-var surnames = randomWizardSurname(WIZARD_SURNAMES);
-
-var randomWizardCoatColor = function (COAT_COLORS) {
-  var coatColor = [];
-  for (var i = 0; i < 4; i++) {
-    coatColor.push(randomizeArray(COAT_COLORS));
-    while (coatColor[i] === coatColor[i + 1] || coatColor[i] === coatColor[i + 2] || coatColor[i] === coatColor[i + 3] || coatColor[i] === coatColor[i - 1] || coatColor[i] === coatColor[i - 2] || coatColor[i] === coatColor[i - 3]) {
-      coatColor.splice(i, 1);
-      coatColor.push(randomizeArray(COAT_COLORS));
-    }
-  }
-  return coatColor;
-};
-
-var coats = randomWizardCoatColor(COAT_COLORS);
-
-var randomWizardEyes = function (EYES_COLORS) {
-  var eyeColor = [];
-  for (var i = 0; i < 4; i++) {
-    eyeColor.push(randomizeArray(EYES_COLORS));
-    while (eyeColor[i] === eyeColor[i + 1] || eyeColor[i] === eyeColor[i + 2] || eyeColor[i] === eyeColor[i + 3] || eyeColor[i] === eyeColor[i - 1] || eyeColor[i] === eyeColor[i - 2] || eyeColor[i] === eyeColor[i - 3]) {
-      eyeColor.splice(i, 1);
-      eyeColor.push(randomizeArray(EYES_COLORS));
-    }
-  }
-  return eyeColor;
-};
-
-var eyes = randomWizardEyes(EYES_COLORS);
-
-var coats = randomWizardCoatColor(COAT_COLORS);
+var randomNames = randomizeArray(WIZARD_NAMES, WIZARDS_TOTAL);
+var randomSurnames = randomizeArray(WIZARD_SURNAMES, WIZARDS_TOTAL);
+var randomCoatColors = randomizeArray(COAT_COLORS, WIZARDS_TOTAL);
+var randomEyesColors = randomizeArray(EYES_COLORS, WIZARDS_TOTAL);
 
 var wizardsFullNames = [];
 
-for (var i = 0; i < 4; i++) {
-  wizardsFullNames.push(names[i] + ' ' + surnames[i]);
-};
+for (var i = 0; i < WIZARDS_TOTAL; i++) {
+  wizardsFullNames.push(randomNames[i] + ' ' + randomSurnames[i]);
+}
 
 var wizards = [
   {
     name: wizardsFullNames[0],
-    coatColor: coats[0],
-    eyesColor: eyes[0]
+    coatColor: randomCoatColors[0],
+    eyesColor: randomEyesColors[0]
   },
   {
     name: wizardsFullNames[1],
-    coatColor: coats[1],
-    eyesColor: eyes[1]
+    coatColor: randomCoatColors[1],
+    eyesColor: randomEyesColors[1]
   },
   {
     name: wizardsFullNames[2],
-    coatColor: coats[2],
-    eyesColor: eyes[2]
+    coatColor: randomCoatColors[2],
+    eyesColor: randomEyesColors[2]
   },
   {
     name: wizardsFullNames[3],
-    coatColor: coats[3],
-    eyesColor: eyes[3]
+    coatColor: randomCoatColors[3],
+    eyesColor: randomEyesColors[3]
   }
 ];
 
