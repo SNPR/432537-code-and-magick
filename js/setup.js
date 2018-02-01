7aae116267b087f32c64cc46a70a7faac1200fd2
snpr <vaugk13@gmail.com>
2018-02-01 20:24:04 +0500

    Переименовал функцию, возвращающую случайное значение массива, переместил код, работающий с DOM-деревом вниз

--- js/setup.js
+++ js/setup.js
@@ -1,25 +1,21 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
-var randomize = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+var randomizeArray = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
 };
-
 
 var randomWizardName = function (WIZARD_NAMES) {
   var name = [];
   for (var i = 0; i < 4; i++) {
-    name.push(randomize(WIZARD_NAMES));
+    name.push(randomizeArray(WIZARD_NAMES));
     while (name[i] === name[i + 1] || name[i] === name[i + 2] || name[i] === name[i + 3] || name[i] === name[i - 1] || name[i] === name[i - 2] || name[i] === name[i - 3]) {
       name.splice(i, 1);
-      name.push(randomize(WIZARD_NAMES));
+      name.push(randomizeArray(WIZARD_NAMES));
     }
   }
   return name;
@@ -31,10 +27,10 @@ var names = randomWizardName(WIZARD_NAMES);
 var randomWizardSurname = function (WIZARD_SURNAMES) {
   var surname = [];
   for (var i = 0; i < 4; i++) {
-    surname.push(randomize(WIZARD_SURNAMES));
+    surname.push(randomizeArray(WIZARD_SURNAMES));
     while (surname[i] === surname[i + 1] || surname[i] === surname[i + 2] || surname[i] === surname[i + 3] || surname[i] === surname[i - 1] || surname[i] === surname[i - 2] || surname[i] === surname[i - 3]) {
       surname.splice(i, 1);
-      surname.push(randomize(WIZARD_SURNAMES));
+      surname.push(randomizeArray(WIZARD_SURNAMES));
     }
   }
   return surname;
@@ -45,10 +41,10 @@ var surnames = randomWizardSurname(WIZARD_SURNAMES);
 var randomWizardCoatColor = function (COAT_COLORS) {
   var coatColor = [];
   for (var i = 0; i < 4; i++) {
-    coatColor.push(randomize(COAT_COLORS));
+    coatColor.push(randomizeArray(COAT_COLORS));
     while (coatColor[i] === coatColor[i + 1] || coatColor[i] === coatColor[i + 2] || coatColor[i] === coatColor[i + 3] || coatColor[i] === coatColor[i - 1] || coatColor[i] === coatColor[i - 2] || coatColor[i] === coatColor[i - 3]) {
       coatColor.splice(i, 1);
-      coatColor.push(randomize(COAT_COLORS));
+      coatColor.push(randomizeArray(COAT_COLORS));
     }
   }
   return coatColor;
@@ -59,10 +55,10 @@ var coats = randomWizardCoatColor(COAT_COLORS);
 var randomWizardEyes = function (EYES_COLORS) {
   var eyeColor = [];
   for (var i = 0; i < 4; i++) {
-    eyeColor.push(randomize(EYES_COLORS));
+    eyeColor.push(randomizeArray(EYES_COLORS));
     while (eyeColor[i] === eyeColor[i + 1] || eyeColor[i] === eyeColor[i + 2] || eyeColor[i] === eyeColor[i + 3] || eyeColor[i] === eyeColor[i - 1] || eyeColor[i] === eyeColor[i - 2] || eyeColor[i] === eyeColor[i - 3]) {
       eyeColor.splice(i, 1);
-      eyeColor.push(randomize(EYES_COLORS));
+      eyeColor.push(randomizeArray(EYES_COLORS));
     }
   }
   return eyeColor;
@@ -81,18 +77,25 @@ for (var i = 0; i < 4; i++) {
 var wizards = [
   {
     name: wizardsFullNames[0],
-    coatColor: coats[0]
+    coatColor: coats[0],
+    eyesColor: eyes[0]
   },
   {
     name: wizardsFullNames[1],
-    coatColor: coats[1]
+    coatColor: coats[1],
+    eyesColor: eyes[1]
   },
   {
     name: wizardsFullNames[2],
-    coatColor: coats[2]
+    coatColor: coats[2],
+    eyesColor: eyes[2]
   },
   {
     name: wizardsFullNames[3],
-    coatColor: coats[3]
+    coatColor: coats[3],
+    eyesColor: eyes[3]
   }
 ];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
